416de9ae7a00e571f96c8b1a0fe185b754669364
OganyanRV <47751491+OganyanRV@users.noreply.github.com>
2020-10-29 04:13:46 +0300

    v2.1 reworked the structure of project

--- Triangular Matrix/samples/sample_matrix.cpp
+++ Triangular Matrix/samples/sample_matrix.cpp
@@ -1,23 +1,25 @@
-#include "../include/TMatrix.h"
-#include "../include/TVector.h"
+﻿  // Copyright 2020 Oganyan Robert
+
+#include <iostream>
+#include "../include/utmatrix.h"
 
 
 int main() {
-  TVector<int> kek(1,0);
-  //TMatrix<int> a(5), b(5), c(5);
- // std::size_t i, j;
-//  std::cout << "Triangular Matrix test" << std::endl;
- // for (i = 0; i < 5; i++) {
-  //  for (j = i; j < 5; j++) {
-    //  a[i][j] = i * 10 + j;
-     // b[i][j] = (i * 10 + j) * 100;
-   // }
- // }
- // c = a + b;
- // auto d = a-b;
- // std::cout << "Matrix a = " << std::endl << a << std::endl;
- // std::cout << "Matrix b = " << std::endl << b << std::endl;
- // std::cout << "Matrix c = a + b" << std::endl << c << std::endl;
-  //std::cout << "Matrix d = a - b" << std::endl << d << std::endl;
+  TMatrix<int> a(5), b(5), c(5);
+  int i, j;
+  std::cout << "Triangular Matrix test" << std::endl;
+  for (i = 0; i < 5; i++) {
+    for (j = i; j < 5; j++) {
+      a[i][j] = i * 10 + j;
+      b[i][j] = (i * 10 + j) * 100;
+    }
+  }
+  c = a + b;
+  auto d = a - b;
+  std::cout << "Matrix a = " << std::endl << a << std::endl;
+  std::cout << "Matrix b = " << std::endl << b << std::endl;
+  std::cout << "Matrix c = a + b" << std::endl << c << std::endl;
+  std::cout << "Matrix d = a - b" << std::endl << d << std::endl;
+  return 0;
 }
-//---------------------------------------------------------------------------
+
